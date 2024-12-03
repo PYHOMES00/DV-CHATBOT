ac72a3e9dad6bccb8e5545fe5e8ec71b32fb439c
nothing... <161198246+PYHOMES00@users.noreply.github.com>
2024-12-04 02:57:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "28243586"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "4022d5686b9b7a7cf8891205921a0ab3"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1679112664"))
+OWNER_ID = int(getenv("OWNER_ID", "7357726710"))
 SUPPORT_GRP = "+cXIPgHSuJnxiNjU1"
 UPDATE_CHNL = "ai_image_junction"
-OWNER_USERNAME = "OfficialDurgesh"
+OWNER_USERNAME = "jhqwgugdwu"
